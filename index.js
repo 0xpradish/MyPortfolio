0d3728cb29ac7a75a505e78b0e16e422ca8e7109
0xpradish <pradish2125@gmail.com>
2023-01-09 00:03:00 +0530

    added projects

--- index.js
+++ index.js
@@ -15,11 +15,35 @@ let displayItems1= `<p id="about-el" class="aboutclass">
 displayarr.push(displayItems1)
 
 
-let displayItems2= ` I am a third-year Artificial Intelligence student at Karpagam Academy of Higher Education.</br> In addition to my studies, I am very interested in blockchain technology and am constantly seeking opportunities to learn more about it.
-// I play cricket and basketball too.`
+let displayItems2= `<div class="aboutmeclass"> <p id="about-me">I am a third-year Artificial Intelligence student at Karpagam Academy of Higher Education. In addition to my studies, I am very interested in blockchain technology and am constantly seeking opportunities to learn more about it.
+I play cricket and basketball too.</p> 
+<ul>
+<li>Java</li>
+<li>C</li>
+<li>Javascript</li>
+<li>CSS</li>
+<li>HTML</li>
+<li>MySQL</li>
+<li>Blockchain</li>
+</ul>
+</div>
+`
 displayarr.push(displayItems2)
 
-let displayItems3= `<div class="projectsdisplay"><h1 id="the-grid">THE GRID</h1>    <h1 id="the-grid">SDG</h1><br> <br> </div>Publications <br> <br><p id="pub-el">Blockchain Technologies: Smart Contracts for Consumer Electronics Data Sharing and Secure Payment</p>`
+let displayItems3= `<div class="myprojects">
+
+
+<h1 id="gridtitle">THE GRID</h1>
+
+<p id="gridinfo">The Grid is a metaverse built on the Polygon Blockchain that provides a virtual environment for business to list and sell their goods.
+Using the grid,customers can explore products with VR glasses and purchase them using a web3wallet<br>
+ <a id="mygridlink"href="www.google.com">click me to view grid</a>
+</p>
+
+
+
+
+</div>`
 displayarr.push(displayItems3)
 
 let clicks=1
